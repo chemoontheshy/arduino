ddf3ffca7037c78a56439b76631cecfd59c50011
chemoontheshy <xzfandzgx@gmail.com>
2020-03-31 23:59:09 +0800

    2

---
+++ bt05/bt05.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("ready");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)//串口接收到数据
+  {
+    String data;
+    while (Serial.available() > 0) {
+      data = data + char(Serial.read());
+      delay(2);
+    }
+    Serial.println("data:" + data);
+  }
+}
