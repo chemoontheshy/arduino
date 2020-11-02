95a7ef7f1a47012d5077db56e3f3fb54eb54177e
chemoontheshy <xzfandzgx@gmail.com>
2020-11-02 11:23:36 +0800

    1

--- Threadlock/Threadlock.ino
+++ Threadlock/Threadlock.ino
@@ -1,17 +1,18 @@
-#define PIN 3
-#define switchA 6
+#define PIN 3 //输出针脚
+#define switchA 6 //输入针脚（接微动开关）
 String data;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(115200);
   pinMode(PIN, OUTPUT);
-  pinMode(switchA, INPUT_PULLUP);
+  pinMode(switchA, INPUT_PULLUP);//INPUT_PULLUP自带上拉不需要自己另外接上位电阻
   //digitalWrite(PIN, HIGH);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   int val = digitalRead( switchA);
+  //可以通过触碰（微动）开关获取一个信号，改变灯的开关
   if (val == HIGH) {
     digitalWrite(PIN, LOW);
     Serial.println("LOW");
@@ -19,6 +20,7 @@ void loop() {
     digitalWrite(PIN, HIGH);
     Serial.println("HIGH");
   }
+  //可以通过串口控制
   if (Serial.available() > 0) {
     receive();
     if (data == "a") {
